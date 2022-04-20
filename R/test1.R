c110fcd95ef275b13d1ec799894bfc66da3cb0b0
Kathryn Doering <kathryn.doering@noaa.gov>
2022-04-20 10:18:19 -0400

    Add a new function

--- R/test1.R
+++ R/test1.R
@@ -9,3 +9,17 @@ add1 <- function(x, y) {
     final <- x + y
     final
 }
+
+#' Add 3 numbers
+#'
+#' Function adds together 2 numbers
+#' @param x The first number
+#' @param y The second number
+#' @param z The third number
+#' @return A single value that is the sum of the 3 numbers
+#' @ export
+#' @author Kathryn
+add_3_nums <- function(x, y, z){
+    final <- x+y+z
+    final
+}
