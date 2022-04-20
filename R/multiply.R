006ce649f29d8129bddcfdf7ea84ea3c9752c1e5
Kathryn Doering <kathryn.doering@noaa.gov>
2022-04-20 10:07:47 -0400

    add simple r pkg files

---
+++ R/multiply.R
@@ -0,0 +1,11 @@
+#' Multiply 2 numbers
+#'
+#' This function multiplies 2 numbers together
+#' @param x The first number to multiply
+#' @param y The second number to multiply
+#' @returns THe multiplication of the 2 numbers
+#' @export
+multiply1 <- function(x, y) {
+    final <- x*y
+    final
+}
