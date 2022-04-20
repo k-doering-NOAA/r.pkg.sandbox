39c1e2efcdb1fc5ab442d852fe5ef60ebbb587b7
Kathryn Doering <kathryn.doering@noaa.gov>
2022-04-20 13:37:45 -0400

    one more fxn

--- R/test1.R
+++ R/test1.R
@@ -1,11 +1,40 @@
 #' addition function
-#' 
+#'
 #' This function adds together 2 numbers
 #' @param x The first value to add
 #' @param y The second value to add
 #' @return A single value that is the sum of the 2 numbers
 #' @export
 add1 <- function(x, y) {
-    final <- x + y
+  final <- x + y
+  final
+}
+
+#' Add 3 numbers
+#'
+#' Function adds together 2 numbers
+#' @param x The first number. Once again
+#' @param y The second number
+#' @param z The third number
+#' @return A single value that is the sum of the 3 numbers
+#' @ export
+#' @author Kathryn
+add_3_nums <- function(x, y, z){
+    final <- x+y+ z
+    final
+}
+
+#' Add 4 numbers
+#'
+#' Function adds together 2 numbers
+#' @param x The first number. Once again
+#' @param y The second number
+#' @param z The third number
+#' @param a The last number
+#' @return A single value that is the sum of the 3 numbers
+#' @ export
+#' @author Kathryn
+add_3_nums <- function(x, y, z, a){
+    final <- x+y+ z + a
     final
 }
