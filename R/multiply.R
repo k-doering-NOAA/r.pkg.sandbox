0690d9db8717e8f650b0545bee192e0f447ca073
k-doering-NOAA <k-doering-NOAA@users.noreply.github.com>
2023-10-13 16:14:58 +0000

    style: run styler::style_pkg()

--- R/multiply.R
+++ R/multiply.R
@@ -1,11 +1,11 @@
 #' Multiply 2 numbers together
 #'
 #' This function multiplies 2 numbers together
-#' @param x The first number to multiply. 
+#' @param x The first number to multiply.
 #' @param y The second number to multiply.
 #' @returns THe multiplication of the 2 numbers
 #' @export
 multiply1 <- function(x, y) {
-  final <- x * y +  1
+  final <- x * y + 1
   final # comment
 }
