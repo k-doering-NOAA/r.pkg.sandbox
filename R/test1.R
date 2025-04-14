83c780021b215fd6606772a3533fa51461c841a4
Kathryn Doering <kathryn.doering@noaa.gov>
2025-04-14 21:41:57 +0000

    test a change

--- R/test1.R
+++ R/test1.R
@@ -3,7 +3,7 @@
 #' This function adds together 2 numbers
 #' @param x The first value to add
 #' @param y The second value to add
-#' @return A single value that is the sum of the 2 numbers
+#' @return A single value that is the sum of the 2 numbers.
 #' @export
 add1 <- function(x, y) {
   final <- x + y
