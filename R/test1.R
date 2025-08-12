82af0f72540d4b66ba22686c6b0ded71b0fc4813
k-doering-NOAA <48930335+k-doering-NOAA@users.noreply.github.com>
2025-08-12 23:04:18 +0000

    style and docs: run devtools::document() and styler::style_pkg()

--- R/test1.R
+++ R/test1.R
@@ -5,10 +5,10 @@
 #' @param y The second value to add
 #' @return A single value that is the sum of the 2 numbers
 #' @export
-add1 <- function(x, y ) {
+add1 <- function(x, y) {
   final <- x + y
   # add some extra code
-  if (final == 2 ) {
+  if (final == 2) {
     z <- 1
   }
   final
