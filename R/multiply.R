c872e9f163165f1a13ab7502afb83168f166c1fd
Kathryn Doering <kathryn.doering@noaa.gov>
2025-08-12 14:07:47 -0700

    change to test

--- R/multiply.R
+++ R/multiply.R
@@ -2,7 +2,7 @@
 #'
 #' This function multiplies 2 numbers together
 #' @param x The first number to multiply. 
-#' @param y The second number to multiply.
+#' @param y The second number to multiply...
 #' @returns THe multiplication of the 2 numbers
 #' @export
 multiply1 <- function(x, y) {
